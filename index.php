6bd4c4e7cadf7a842dbb7a0243fadf022a2784fe
Jenn Vargas <foreverdigital@gmail.com>
2017-01-24 18:12:54 -0500

    copy updates

--- index.php
+++ index.php
@@ -30,69 +30,80 @@
   </div>
   <div class="row">
     <div class="col-xs-12">
-		<p>Welcome to Mostly Brilliant, an independent Product and UX consultancy for companies that want to build great products. I specialize in helping businesses translate their big vision for a product into a user-focused, executable plan that minimizes waste in the development process.</p>
-      
-		<h2>Who am I?</h2>
-		<p>&#128075; I'm <a href="http://jennvargas.com">Jenn Vargas</a>. I have over 8 years of product management and UX experience ranging from giant companies to brand new startups. I've done social, e-commerce, editorial & content, mobile, desktop, SaaS, you name it. I focus on product strategy, product development, and user experience with the goal of helping you eliminate wasted resources as you build out your digital products. <a href="http://linkedin.com/in/jennifervargas">Here's my LinkedIn</a> if you'd like the nitty-gritty.</p>
+		<p>Welcome to Mostly Brilliant, an independent Product and UX consultancy focused on helping your small business unlock its potential through design, process, and product improvements. I specialize in helping you translate your big vision into a customer-focused, executable plan that optimizes for maximum impact.</p>
 
 	<!--	<h2>What do I do?</h2>
 		<p>I help companies get products out of their heads and into the world. I take your Big Idea or Big Problem, analyze it, and turn it into executable bites strategically designed to reach your specific goals. I believe an experience-driven, user-focused approach is the foundation of a successful product.</p>
 		<p>Often companies jump straight from an idea into design and development without strategically defining their product and how it might best balance the needs of the business with the needs of their target users. This leads to waste of time, money, and effort. That's where I come in. </p>
 		<p>My goal is to take your vision for your product, balance the user and business needs, and design an optimal solution for your particular business that is ready for design and development. Your product strategy will be designed for the real world.</p> -->
-      	
-      	<h2>About Mostly Brilliant</h2>
-      	<p>Mostly Brilliant is an independent consultancy. That means it's just me on the other side of this keyboard making all the magic happen. I've built a reputation for being able to do the work of many, do it efficiently, and do it well.</p>
-		<p>Recently, I've worked with:</p>
-		<ul>
-      <li>A collaborative social media planning and scheduling application. (Web)</li>
-      <li>An Uber-like service targeted at college students. (Mobile)</li>
-		  <li>A crowd-powered beauty service discovery and booking platform. (Mobile)</li>
-		  <li>A mindfulness app focused on breathing and stress-reduction. (Web)</li>
-      <li>An app to facilitate the mentorship process. (Web)</li>
-		</ul>
+  
 
-		<p>Past clients include ClassPass, Kandu (now Katapult), and Over.</p>
+    <h2>Services</h2>
 
-		<p>Mostly Brilliant is a proudly location-independent company. My office is wherever I am and I've worked with clients all over the world: New York, London, Cape Town, and Nashville, to name a few. You could be next!
+    <h3>Product & UX Audit</h3>
+    <span class="pricing">$1000</span>
+    <p>Congrats! Your product is out in the world! But what's next? You have a to-do list a mile long, but don't know the best place to start. I'll do an audit of your product with a focus on user experience and usability. From this, you'll get a report of at least 15 opportunities for improvement and optimization to give your users the best experience possible leading to increased conversion and retention.</p>
 
-      <h2>Services</h2>
+    <p>A glowing review from Kate Kendall of <a href="http://cloudpeeps.com">CloudPeeps</a>:</p>
 
-      <h3>Product & UX Audit</h3>
-      <span class="pricing">$1000. (Average 2 business-day turnaround.)</span>
-      <p>Congrats! Your product is out in the world! But what's next? You have a to-do list a mile long, but don't know the best place to start. I'll do an audit of your product with a focus on user experience and usability. From this, you'll get a report of at least 15 opportunities for improvement and optimization to give your users the best experience possible leading to increased conversion and retention.</p>
+    <div class="review-img">
+      <img src="review-kk.png" alt="A glowing review from Kate Kendall of CloudPeeps" />
+    </div>
+    <p><strong>Current turnaround time for your UX Audit:</strong> Less than 2 business days.</p>
+    <form action="/success" method="POST" class="inline-button">
+    <input type="hidden" name="product-id" value="audit" />
+      <script
+        src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+        data-key="pk_live_BAkno0Yvw1qLJhEbJyBhLjT2"
+        data-amount="100000"
+        data-name="Mostly Brilliant"
+        data-description="Product & UX Audit"
+        data-image="https://s3.amazonaws.com/stripe-uploads/qtrIgOTMPDHYNzREgfe2cL4MTyeu2pETmerchant-icon-1473110340448-square-logo.png"
+        data-locale="auto"
+        data-panel-label="Place order"
+        data-label="Book your Audit now"
+        data-bitcoin="true">
+      </script>
+    </form>
 
-      <form action="/success" method="POST" class="inline-button">
-      <input type="hidden" name="product-id" value="audit" />
-        <script
-          src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-          data-key="pk_fU3W7AlFlEEupRqXmPWAZbharLLph"
-          data-amount="100000"
-          data-name="Mostly Brilliant"
-          data-description="Product & UX Audit"
-          data-image="https://s3.amazonaws.com/stripe-uploads/qtrIgOTMPDHYNzREgfe2cL4MTyeu2pETmerchant-icon-1473110340448-square-logo.png"
-          data-locale="auto"
-          data-panel-label="Place order"
-          data-label="Order your Audit now"
-          data-bitcoin="true">
-        </script>
-      </form>
+    <h3>On-Demand Product & UX Expert</h3>
+    <span class="pricing">Full Day: $1200 / Half Day: $650</span>
+    <p>If you have a short term or interim project (or worse: a product or design emergency!) get in touch! Subject to availability, you can count on me to get the job done. No commitments, no sales &mdash; just the solution you need, when and where you need it.</p>
 
-      <h3>On-Demand Product & UX Expert</h3>
-        <span class="pricing">Full Day: $1200 / Half Day: $650</span>
-        <p>If you have a short term or interim project, let me know! Subject to availability, I am free for consultation. No commitments, no sales &mdash; just straightforward advice when and where you need it. When these buttons are active, you'll hear from me within 24 hours (usually quicker) and we'll get your urgent request underway!</p>
-
-	    <h3>Product & UX Advisor</h3>
+    <h3>Product & UX Advisor</h3>
     <span class="pricing">$1600 per month, minimum 3-month commitment.</span>
-    <p>You have a lot to worry about when building your business. When you're making big decisions about your product, what features should you prioritize? How can you make sure you're using your often limited resources in the best way? You're not ready to hire a full-time Product or UX person, but you definitely need someone to help things move along. With this package, I'll learn about your business, your goals, and help develop a roadmap and KPIs for you to reach those goals. We'll have one kickoff meeting to develop the plan and then monthly check-ins to make sure things are on track and adjust where needed. I'll be available for any questions or consultations you may need over the course of our engagement. Perfect for when you're in the "in between" phase of your business.</p>
+    <p>You have a lot to worry about when building your business. When you're making big decisions about your product, what features should you prioritize? How can you make sure you're using your often-limited resources in the best way? You're not ready to hire a full-time Product or UX person, but you definitely need someone to help things move along.</p>
+    <p>With this package, I'll learn about your business, your goals, and help develop a roadmap and KPIs for you to reach those goals. We'll have a kickoff meeting to develop a plan and then monthly check-ins to make sure things are on track. I'll be available for any questions or consultations you may need over the course of our engagement. Perfect for when you're in the "in between" phase of your business.</p>
 
     <h3>Product Development</h3>
     <p>On a case-by-case basis, I work with companies and individuals to indentify and build out an MVP of a new product. Please contact me via the form below for more information.</p>
 
-      <h2>You + Mostly Brilliant</h2>
-      <p>I'm looking for awesome clients with interesting projects. You can be <span class="highlight">anywhere in the world</span>. For us to work together well, you need to be <span class="highlight">comfortable with digital communication</span>. If the words Slack, screenshot, Google Docs, or emoji make you shudder, we may not be a very good fit. I like to enjoy the process of work, and that may involve the occasional gif. I hope you do too.</p>
+    <h3>Questions?</h3>
+    <p>Happy to help! <a href="mailto:hi@mostlybrilliant.co">Just shoot me a quick email.</a></p>
+
+    <h2>About Mostly Brilliant</h2>
+    <p>Mostly Brilliant is a proudly independent consultancy. That means it's just me on the other side of this keyboard making all the magic happen. I've built a reputation for being able to do the work of many, do it efficiently, and do it well.</p>
+    <p>Recently, I've worked with:</p>
+    <ul>
+      <li>A collaborative social media planning and scheduling application. (Web)</li>
+      <li>An Uber-like service targeted at college students. (Mobile)</li>
+      <li>A crowd-powered beauty service discovery and booking platform. (Mobile)</li>
+      <li>A mindfulness app focused on breathing and stress-reduction. (Web)</li>
+      <li>An app to facilitate the mentorship process. (Web)</li>
+    </ul>
+
+    <p>Past clients include ClassPass, Kandu (now Katapult), and Over.</p>
+
+    <p>Mostly Brilliant is a location-independent company. My office is wherever I am and I've worked with clients all over the world: New York, London, Cape Town, and Nashville, to name a few. You could be next!</p>
       
-      <h2>Need more info?</h2>
-      <p>👍 Enter your email and I'll be in touch shortly. I look forward to learning more about your project.</p>
+      <h2>Who am I?</h2>
+    <p>&#128075; I'm <a href="http://jennvargas.com">Jenn Vargas</a>. I have over 8 years of product development and UX experience ranging from giant companies to brand new startups. I've done social, e-commerce, editorial & content, mobile, desktop, SaaS, you name it. I focus on strategy, product development, and user experience with the goal of helping you eliminate wasted resources as you build out your business. <a href="http://linkedin.com/in/jennifervargas">Here's my LinkedIn</a> if you'd like the nitty-gritty.</p>
+
+    <h2>You + Mostly Brilliant</h2>
+      <p>I'm looking for awesome clients with interesting projects. You can be <span class="highlight">anywhere in the world</span>. For us to work together well, you need to be <span class="highlight">comfortable with digital communication</span>. If the words Slack, screenshot, Google Docs, or emoji make you shudder, we may not be a very good fit. I like to enjoy the process of work, and that may involve the occasional gif. I hope you do too.</p>
+
+      <h2>Let's Work Together</h2>
+      <p>Enter your email and I'll be in touch shortly. I look forward to learning more about your project.</p>
       <!-- Begin MailChimp Signup Form -->
     <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
     <style type="text/css">
@@ -107,7 +118,7 @@
         <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="you@awesome.com" required>
           <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
           <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_4cca0202faa1d268df2007d72_92eb669271" tabindex="-1" value=""></div>
-          <div class="clear"><input type="submit" value="Say Hi" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+          <div class="clear"><input type="submit" value="Get Started" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
           </div>
       </form>
     </div>
@@ -119,7 +130,7 @@
 <footer class="container">
   <div class="row">
     <div class="col-xs-12">
-      <p class="footer-meta">&copy; 2016 Mostly Brilliant</p>
+      <p class="footer-meta">&copy; 2017 Mostly Brilliant</p>
     </div>
   </div>
   <?php require_once("/feedback-popup.php"); ?>
