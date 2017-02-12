f4abd6a56bdcdded167c8bdd1eb67604ab899b27
Jenn Vargas <foreverdigital@gmail.com>
2017-02-12 01:02:10 -0500

    adds live design audit page

---
+++ live-audit-landing.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Live Website Audits &mdash; Mostly Brilliant &mdash; An Independent Product & UX Consultancy</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+        <link href='https://fonts.googleapis.com/css?family=Hammersmith+One|Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="/style.css">
+
+        <meta property="og:url" content="http://mostlybrilliant.co/live-audit/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="FREE Live Website Design Audits &mdash; Mostly Brilliant" />
+        <meta property="og:description" content="Know you need to improve your site but don't know where to begin? Each month I select up to 5 websites and perform a design and usability audit while answering your questions LIVE." />
+        <meta property="og:image" content="http://mostlybrilliant.co/img/live-design-audit.png" />
+
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+      <!-- ngIf: app.state -->
+
+<div id="audit-landing-page" class="container">
+  <div class="row">
+    <div class="col-xs-12">
+      <h1 id="logo"><a href="/"><img src="/site-header.png" alt="Mostly Brilliant" /></a></h1>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-xs-12">
+		<h1>Improve Your Site in 15 Minutes</h1>
+    <h2 class="text-center">Join My Live Website Design Audits</h2>
+    <p>You know you need to improve the design and usability of your site, but you have no idea where to begin, right?</p>
+    <p>Putting together a site that looks good and achieves its goals is not rocket science stuff. You just have to know what to look for.</p>
+    <p>It boils down to some basic design principles. You can go a long way with the right mix of color, typography, positioning, and hierarchy. Even if you aren't a designer. Or even if "designing" intimidates the hell out of you. </p>
+
+    <h3 class="text-center">I'll show you how I do it in 15 minutes</h3>
+    <p>Hey! I'm Jenn. Every month, I do a live stream where I audit up to 5 submitted sites, adjusting the design in real time, talking through the "why" and answering your questions. This is a big part of what I do as a consultant and it's one of my favorite services because it's such an easy way to have a big impact on your site's performance. You'll see the site change before your eyes without using any fancy tools. In fact, you already have the tools you need.</p>
+
+    <h3 class="text-center">Here's a quick live-audit example:</h3>
+    <p>Beck's landing page for <a href="http://nomadfly.me" target="_blank">NomadFly</a> had a lot going for it, but there was no hierarchy for the text and the important calls to action were in conflict. Here's a quick design audit of just the first view of the page:</p>
+    <div id="previews" class="row">
+      <div class="col-xs-12 col-sm-6">
+        <h5>BEFORE</h5>
+        <img src="img/nomadfly-before.png" alt="Nomadfly Before" />
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <h5>AFTER</h5>
+        <img src="img/nomadfly-after.png" alt="Nomadfly After" />
+      </div>
+    </div>
+
+    <h3 class="text-center">I charge $1000 for an Audit. You can join for FREE.</h3>
+    <p>My <a href="http://mostlybrilliant.co/ux-audit">UX audits</a> are comprehensive reports about how to improve your site's design and usability to reach your goals. In the live stream, I do an abbreviated version of this for FREE. Anyone can attend the live sessions and ask questions. If you're interested in improving your own site or if you're curious about design and UX, join us!</p>
+
+    <h3 class="text-center">Why give it away for free?</h3>
+    <p>Because it's fun :) And because I want the internet to be a more beautiful and usable place. That starts with more people knowing how to do it for themselves!</p>
+
+    <h3 class="text-center">Ready to make your site awesome?</h3>
+    <div class="row">
+    <!-- Begin MailChimp Signup Form -->
+    <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+    <div id="mc_embed_signup" class="live-stream-form col-xs-12 col-sm-10 col-sm-offset-1">
+    <p>Sign up to join the next session:</p>
+      <form action="//mostlybrilliant.us13.list-manage.com/subscribe/post?u=4cca0202faa1d268df2007d72&amp;id=92eb669271&SOURCE=live-audit" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+        <div id="mc_embed_signup_scroll">
+          <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+          <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+          <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_4cca0202faa1d268df2007d72_92eb669271" tabindex="-1" value=""></div>
+          <div class="clear"><input type="submit" value="Join the next session" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+        </div>
+      </form>
+    </div>
+    </div>
+    <!--End mc_embed_signup-->
+    
+    <h3 class="text-center">Hey Jenn, Can you audit my site?</h3>  
+		<p>I review 3-5 sites in each session. If you'd like your site to be included, sign up for the list and when I send out the request for volunteers, be sure to respond ASAP!</p>
+   
+  
+    </div>
+  </div>
+</div>
+
+<footer class="container">
+  <div class="row">
+    <div class="col-xs-12">
+      <p class="footer-meta">&copy; 2014-2017 Mostly Brilliant</p>
+    </div>
+  </div>
+</footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-48251742-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+</html>
