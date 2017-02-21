95f7d7b5064201d2e3d47e8786ed1ad0bb3a0e62
Jenn Vargas <foreverdigital@gmail.com>
2017-02-21 15:41:57 -0500

    copy update

--- includes/promo.php
+++ includes/promo.php
@@ -1,7 +1,7 @@
 <div class="row">
 	<div id="promo-box" class="col-xs-12">
 	    <h4 class="text-center">3rd Anniversary Party!</h4>
-	    <p>Celebrate Mostly Brilliant's 3rd birthday (and this shiny new site) with me! Get a <strong>$300 discount on all<super><small>*</small></super> services booked before March 1.</strong> Hurry - spots are limited!</p>
+	    <p>Celebrate Mostly Brilliant's 3rd birthday (and this shiny new site) with me! Enjoy a <strong>$300 discount on all<super><small>*</small></super> services booked before March 1.</strong> Hurry - spots are limited!</p>
 	    <small class="footnote"><super>*</super> 60-min Consult Calls not eligible.</small>
 	</div>
 </div>
