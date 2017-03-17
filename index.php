4e73d5955a08ce0433de826be5e66f711e2e1b15
Jenn Vargas <foreverdigital@gmail.com>
2017-03-17 13:42:13 -0400

    abstracts the email list signup into a widget

--- index.php
+++ index.php
@@ -130,30 +130,6 @@
     	</div>
 		</div>
 	</div>
-	<div id="sign-up-section">
-		<div class="container">
-			<div class="row">
-				<div class="col-xs-12 col-sm-6 col-sm-offset-3">
-					<!-- Begin MailChimp Signup Form -->
-				    <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
-				    <div id="mc_embed_signup" class="live-stream-form col-xs-12 col-sm-10 col-sm-offset-1">
-				    <h4>Want to try some DIY first?</h4>
-				    <p class="intro">Every month I do a livestreamed <a href="/live-design-audit">abbreviated UX Audit</a> where I demo the process on 3-5 submitted sites and answer your questions in real time, free!</p>
-				    <p>Join the next session:</p>
-				      <form action="//mostlybrilliant.us13.list-manage.com/subscribe/post?u=4cca0202faa1d268df2007d72&amp;id=be387c1a0a&SOURCE=live-audit" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-				        <div id="mc_embed_signup_scroll">
-				          <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-				          <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-				          <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_4cca0202faa1d268df2007d72_be387c1a0a" tabindex="-1" value=""></div>
-				          <div class="clear"><input type="submit" value="Sign me up!" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-				        </div>
-				      </form>
-				    </div>
-				    </div>
-				    <!--End mc_embed_signup-->
-				</div>
-			</div>	
-		</div>
-	</div>
+	<?php require_once('includes/email-list.php'); ?>
 </div>
 <?php require_once("includes/footer.php"); ?>
--- service-optimize-basic.php
+++ service-optimize-basic.php
@@ -23,29 +23,7 @@
     			<p>$1200/mo or $3300/quarter</p>
     		</div>
     	</div>
-    	<div class="row button-full">
-    		<div class="col-xs-12">
-    		  <!-- Begin MailChimp Signup Form -->
-			    <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
-			    <style type="text/css">
-			      #mc_embed_signup{clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
-			      /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
-			         We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-			    </style>
-			    <div id="mc_embed_signup">
-			      <form action="//mostlybrilliant.us13.list-manage.com/subscribe/post?u=4cca0202faa1d268df2007d72&amp;id=92eb669271&SOURCE=site-lead&PRODUCT=optimize-basics" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-			          <div id="mc_embed_signup_scroll">
-			        
-			        <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="you@awesome.com" required>
-			          <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-			          <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_4cca0202faa1d268df2007d72_92eb669271" tabindex="-1" value=""></div>
-			          <div class="clear"><input type="submit" value="Get Started" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-			          </div>
-			      </form>
-			    </div>
-			  <!--End mc_embed_signup-->
-    		</div>
-		</div>
+    	<?php require_once('includes/email-list.php'); ?>
     </div>
     <div class="alt-blue-section">
         <div class="container">
