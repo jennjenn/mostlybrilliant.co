972035a99462a04f6d3e02cf7b2dc2d8baad08ff
Jennifer Vargas <foreverdigital@gmail.com>
2017-06-19 16:19:05 -0400

    adds testimonial to about page

--- about.php
+++ about.php
@@ -10,14 +10,29 @@
     			&#128075; &nbsp;  I'm Jenn Vargas. I have over 10 years of product development and UX experience ranging from large companies like <a href="http://yahoo.com">Yahoo</a> and <a href="http://etsylcom">Etsy</a> to brand new startups trying to establish themselves in the world. Now, I use that experience to help new and growing businesses, like yours, define and refine your digital products.</p>
 				<p>I've been called a unicorn. And a swiss army knife. I'm proud of both. I live for creating order from chaos, especially when it means identifying the most effective path to an end goal using only a paperclip, a piece of gum, and a can-do attitude. Kidding. Sort of.</p>
 				<p>I come with product management, strategy, user experience, and development skills in my back pocket and assess all problems and possibilities through the various lenses that gives me. This means <strong>I'm able to evaluate your needs and understand the impact of strategy and decisions across the various disciplines it'll take to execute your vision.</strong></p>
+
+                <div id="reviews">
+                    <div class="review">
+                        <div class="review-text">
+                            <div class="review-img"><img src="img/nicolesteeger.jpg" alt="Nicole Steeger, GigMasters" /></div>
+                            I worked with Jenn on a fast-paced project with a tight deadline. We knew we needed the perfect person to get it done correctly and on time. Jenn was that person. We couldn't be more pleased with the outcome and finishing a day early! Jenn has a clear and efficient process that would work well for any team. She is a great listener, excellent collaborator, and has the dual expertise of both UX and UI. She looks at everything from a product perspective which really translates to great designs. I would highly recommend Jenn and look forward to working with her again!
+                          <div class="attribution">- Nicole Steeger, Product Team Manager @ <a href="http://gigmasters.com" target="_blank">GigMasters</a></div>
+                        </div>
+                    </div>
+                </div>
+
+                
+
+
                 <p>Some of the awesome companies I've worked with:</p>
                 <ul>
-                    <li><a href="http://classpass.com" target="_blank">ClassPass</a></li>
-                    <li><a href="http://www.katapultstudio.com/" target="_blank">Kandu (now Katapult)</a></li>
-                    <li><a href="http://madewithover.com/" target="_blank">Over</a></li>
-                    <li><a href="http://gun.io" target="_blank">Gun.io</a></li>
-                    <li><a href="http://zaapt.com" target="_blank">Zaapt</a></li>
+                    <li><a href="http://gigmasters.com" target="_blank">GigMasters (part of XO Group)</a></li>
                     <li><a href="http://brooklynbridge.vc" target="_blank">Brooklyn Bridge Ventures</a></li>
+                    <li><a href="http://zaapt.com" target="_blank">Zaapt</a></li>
+                    <li><a href="http://gun.io" target="_blank">Gun.io</a></li>
+                    <li><a href="http://madewithover.com/" target="_blank">Over</a></li>
+                    <li><a href="http://www.katapultstudio.com/" target="_blank">Kandu (now Katapult)</a></li>
+                    <li><a href="http://classpass.com" target="_blank">ClassPass</a></li>
                 </ul>
                 <p>As your product and ux consultant, I <em>love</em> getting into the details and can't help but treat your business as I do my own.</p>
 				<p><a href="http://linkedin.com/in/jennifervargas">Here's my LinkedIn</a> if you'd like the nitty-gritty.</p>	
